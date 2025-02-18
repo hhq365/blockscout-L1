f0630904ed5e366af8d5f4fd36791cb9b7299f14
quentin365 <quentin@likn.co>
2025-02-18 14:49:49 +0800

    filter top account 5

--- apps/block_scout_web/lib/block_scout_web/controllers/api/v2/address_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/v2/address_controller.ex
@@ -839,7 +839,7 @@ defmodule BlockScoutWeb.API.V2.AddressController do
       |> split_list_by_page()
 
     # quentin edit
-    addresses = Enum.reject(addresses, fn {address} ->
+    addresses = Enum.reject(addresses, fn address ->
       to_string(address.hash) == "0x03FBC532815f55B628852AEF37660d4A4894A15B" end)
 
     next_page_params = next_page_params(next_page, addresses, params)
