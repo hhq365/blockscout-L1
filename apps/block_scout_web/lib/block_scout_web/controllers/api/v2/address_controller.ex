5aa477d18893a386ffa7fc2c75230dca778bec13
quentin365 <quentin@likn.co>
2025-02-18 11:38:56 +0800

    try filter top addresses

--- apps/block_scout_web/lib/block_scout_web/controllers/api/v2/address_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/v2/address_controller.ex
@@ -838,6 +838,10 @@ defmodule BlockScoutWeb.API.V2.AddressController do
       |> Address.list_top_addresses()
       |> split_list_by_page()
 
+
+    addresses = Enum.reject(addresses, fn address ->
+      address["hash"] == "0x03FBC532815f55B628852AEF37660d4A4894A15B" end)
+
     next_page_params = next_page_params(next_page, addresses, params)
 
     exchange_rate = Market.get_coin_exchange_rate()
